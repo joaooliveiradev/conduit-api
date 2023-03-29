f38871ecb166451560c20702e931120d5d27bd2f
João Oliveira <joaooliveira1775@gmail.com>
2023-03-29 08:55:00 -0300

    feat: add decode error

--- src/helpers/validate-codec.ts
+++ src/helpers/validate-codec.ts
@@ -1,16 +1,17 @@
-import * as E from 'fp-ts/Either'
-import { pipe } from 'fp-ts/function'
-import { failure } from 'io-ts/PathReporter'
-import { Type } from 'io-ts'
-import { ValidationError } from '@/helpers/errors'
+import * as E from "fp-ts/Either";
+import { pipe } from "fp-ts/function";
+import { failure } from "io-ts/PathReporter";
+import { Type } from "io-ts";
+import { DecodeError } from "@/helpers/errors";
 
-type ValidateCodec = <A, O, I>(codec: Type<A, O, I>) =>
-  (data: I) => E.Either<ValidationError, O>
+type ValidateCodec = <A, O, I>(
+  codec: Type<A, O, I>
+) => (data: I) => E.Either<DecodeError, O>;
 
 export const validateCodec: ValidateCodec = (codec) => (data) => {
   return pipe(
     codec.decode(data),
     E.map(codec.encode),
-    E.mapLeft(errors => new ValidationError(failure(errors).join(':::'))),
-  )
-}
+    E.mapLeft((errors) => new DecodeError(failure(errors).join(":::"))),
+  );
+};
